54a3c4c311159c5afea8bd4bf214a690e698f545
David Wing <wing_david@rocketmail.com>
2024-07-14 19:36:10 -0600

    Added a menubar with reset button

--- src/main/Input.java
+++ src/main/Input.java
@@ -1,14 +1,23 @@
 package main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Input implements KeyListener {
+public class Input implements KeyListener, ActionListener {
 
 	static boolean LEFT = false;
 	static boolean RIGHT = false;
 	static boolean UP = false;
 	static boolean DOWN = false;
+	static boolean RESET = false;
+	
+	private static Input singleton = new Input();
+	
+	public static Input getInput() {
+		return singleton;
+	}
 	
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -16,33 +25,40 @@ public class Input implements KeyListener {
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+		if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
 			Input.LEFT = true;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
 			Input.RIGHT = true;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_UP) {
+		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
 			Input.UP = true;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+		if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
 			Input.DOWN = true;
 		}
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+		if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
 			Input.LEFT = false;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
 			Input.RIGHT = false;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_UP) {
+		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
 			Input.UP = false;
 		}
-		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+		if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
 			Input.DOWN = false;
 		}
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO: Check what kind of action this is
+		
+		// Right now I am just assuming it is the reset action
+		Input.RESET = true;
 	}
 	
 	
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Main {
 	
@@ -21,6 +24,12 @@ public class Main {
 		while(running) {
 			world.update();
 			frame.repaint();
+			
+			// Check if the simulation needs to be reset
+			if (Input.RESET) {
+				world = new World(50,50,25);
+				Input.RESET = false;
+			}
 			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
@@ -35,13 +44,33 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(750, 750);
 		frame.setContentPane(new WorldRenderer());
-		frame.addKeyListener(new Input());
+		frame.addKeyListener(Input.getInput());
+		frame.setJMenuBar(createJMenuBar());
 		frame.setVisible(true);
+	}
+	
+	public static JMenuBar createJMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		
+		///////////////
+		// Create menus
+		///////////////
+		
+		// File menu
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem resetMenuItem = new JMenuItem("Reset");
+		resetMenuItem.addActionListener(Input.getInput());
+		fileMenu.add(resetMenuItem);
+		
+		// Add menus to the menu bar
+		menuBar.add(fileMenu);
+		
+		return menuBar;
 	}
 	
 	public static float distance(Transform t1, Transform t2) {
 		return (float) Math.hypot(t1.x-t2.x, t1.y-t2.y);
-		
 	}
 
 }
